067421b6ec06222719ee8cd42f1b5b8462abd86c
Mate Soos <soos.mate@gmail.com>
2025-04-21 22:45:55 +0200

    Add emscripten build

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-rm -rf cm* CM* lib* pep* Testing* tests* include tests utils Make*
+rm -rf cm* CM* lib* cryptomini* Testing* tests* pycryptosat include tests utils Make*
 emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF ..
 emmake make -j26
 emmake make install
